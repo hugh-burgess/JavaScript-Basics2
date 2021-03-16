caea766017d98da2adfb8d2a61e106b74f44656f
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-16 17:10:12 +0100

    5 complete

--- 4-strings/solution.js
+++ 4-strings/solution.js
@@ -8,3 +8,32 @@ function isEmptyString(a) {
 
 console.log(isEmptyString("hello"));
 console.log(isEmptyString(""));
+
+function isIncluded(a, b) {
+  if (a.includes("us") && b.includes("us")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isIncluded("house", "mouse"));
+console.log(isIncluded("mouse", "dog"));
+
+function firstCharacter(a) {
+  return a.charAt(0);
+}
+
+console.log(firstCharacter("house"));
+
+function lastCharacter(b) {
+  return b.charAt(b.length - 1);
+}
+
+console.log(lastCharacter("house"));
+
+function capitalize(c) {
+  return c[0].toUpperCase() + c.slice(1);
+}
+
+console.log(capitalize("house"));
---
+++ 5-my-bootcamp/solution.js
@@ -0,0 +1,5 @@
+let myArr = ["Alex", "David", "Eduardo", "Hugh", "Johan", "Johannes", "Jörg"];
+
+for (let i = 0; i < myArr.length; i++) {
+  console.log(`#${i + 1} ${myArr[i]}`);
+}
